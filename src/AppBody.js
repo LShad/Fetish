9d8c5fae4edcbac84e49759bd4876af5f1b1c84b
LShad <michal.ratajczak@onet.eu>
2019-05-22 22:05:25 +0200

    Added site body

---
+++ src/AppBody.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+class AppBody extends React.Component{
+	render(){
+		return(
+			<div>
+      			elokurwa
+   			</div>
+		);
+	}
+}
+
+export default AppBody;
