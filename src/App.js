a52dc3d31416c138e152de6eef288faa8af60d1a
monumatik <monumatik@gmail.com>
2019-05-22 23:13:01 +0200

    New header style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header';
 import AppBody from './AppBody';
-
+import './App.css';
 function App() {
   return (
     <React.Fragment>
+    <div className="container-fluid header-container border border-primary">
       <Header/>
+    </div>
       <AppBody/>
     </React.Fragment>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,28 +1,10 @@
 import React from 'react';
-import './Header.css';
 
 class Header extends React.Component{
 	render(){
 		return(
-			<div className="header-contener">
-      			<div className="row">
-        			<div className="col">
-         				<img src="https://scontent-waw1-1.xx.fbcdn.net/v/t1.0-9/60339771_2207981582621007_4250728505785974784_n.png?_nc_cat=1&_nc_ht=scontent-waw1-1.xx&oh=bb917460c6f57e27c7c3bba90dc7fab3&oe=5D99AAC9" 
-         				className="img-fluid" style={{maxHeight: '100px'}}/>
-        			</div>
-        			<div className="col  border border-primary text-right my-auto">
-         				<div className="dropdown">
-						  <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-						    Dropdown button
-						  </button>
-						  <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-						    <a className="dropdown-item" href="#">Action</a>
-						    <a className="dropdown-item" href="#">Another action</a>
-						    <a className="dropdown-item" href="#">Something else here</a>
-						  </div>
-						</div>
-        			</div>
-      			</div>
+			<div className="container">
+      		asdasd
    			</div>
 		);
 	}
