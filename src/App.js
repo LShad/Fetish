fedd3d3dcbf1d9854674daceb2feb1bc773dc917
monumatik <monumatik@gmail.com>
2019-05-23 19:15:36 +0200

    Global layout edit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import Header from './Header';
 import AppBody from './AppBody';
 import './App.css';
+
 function App() {
   return (
     <React.Fragment>
-    <div className="container-fluid header-container border border-primary">
+    <div className="container-fluid header-container border border-primary" style={{position:'fixed'}}>
       <Header/>
     </div>
       <AppBody/>
