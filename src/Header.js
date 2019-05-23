95bbc947ed28f4888bc1df1afbd4152d478d9f9a
LShad <michal.ratajczak@onet.eu>
2019-05-23 20:30:49 +0200

    Added header icon

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,8 @@ import React from 'react';
 class Header extends React.Component{
 	render(){
 		return(
-			<div className="container border border-primary">
-      		asdasd
+			<div className="container border bg-white">
+      		<img src="1.png" className="img-fluid" width="200" height="200" alt="Fetish"/>
    			</div>
 		);
 	}
