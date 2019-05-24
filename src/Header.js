add470880d6b6b0424ba902c0ccdb47a2c99988e
LShad <michal.ratajczak@onet.eu>
2019-05-24 18:23:44 +0200

    Added new icon and repaired scaling of icons

--- src/Header.js
+++ src/Header.js
@@ -3,18 +3,20 @@ import React from 'react';
 class Header extends React.Component{
 	render(){
 		return(
-			<div className="container border bg-white">
+			<div className="container bg-white">
 				<div className="row align-items-center">
 					<div className="col">
    						<div className="d-flex flex-row bd-highlight">
-   							<img src="logo.png" className="img-fluid" style={{width: "12rem", height: "4.5rem"}} alt="Fetish"/>
+   							<img src="logo.png" className="img-fluid" style={{width: "40%", height: "40%"}} alt="Fetish"/>
 						</div>
 					</div>
 					<div className="col">
 						<div className="d-flex flex-row-reverse bd-highlight h-100 ">
-							<img src="logo.png" className="img-fluid" style={{width: "7rem", height: "2.5rem"}} alt="Fetish"/>
-			  				<img src="logo.png" className="img-fluid" style={{width: "7rem", height: "2.5rem"}} alt="Fetish"/>
-			  				<img src="logo.png" className="img-fluid" style={{width: "7rem", height: "2.5rem"}} alt="Fetish"/>
+							<img src="heart.png" className="img-fluid" style={{width: "5%", height: "5%"}} alt="Settings"/>
+							<img src="empty.png" className="img-fluid" style={{width: "5%", height: "5%"}} alt=""/>
+			  				<img src="heart.png" className="img-fluid" style={{width: "5%", height: "5%"}} alt="Notifications"/>
+			  				<img src="empty.png" className="img-fluid" style={{width: "5%", height: "5%"}} alt=""/>
+			  				<img src="heart.png" className="img-fluid" style={{width: "5%", height: "5%"}} alt="Heart"/>
 			  			</div>
 					</div>
 				</div>
