5aecaae810cba76bb736c10aecf28385bc26bbc7
LShad <michal.ratajczak@onet.eu>
2019-05-24 17:00:08 +0200

    Repaired scaling of images

--- src/Header.js
+++ src/Header.js
@@ -7,14 +7,14 @@ class Header extends React.Component{
 				<div className="row align-items-center">
 					<div className="col">
    						<div className="d-flex flex-row bd-highlight">
-   							<img src="1.png" className="img-fluid" width="200"  alt="Fetish"/>
+   							<img src="1.png" className="img-fluid" width="35%" height="35%" alt="Fetish"/>
 						</div>
 					</div>
 					<div className="col">
 						<div className="d-flex flex-row-reverse bd-highlight h-100 ">
-							<img src="1.png" className="img-fluid" width="100" alt="Fetish"/>
-			  				<img src="1.png" className="img-fluid" width="100" alt="Fetish"/>
-			  				<img src="1.png" className="img-fluid" width="100" alt="Fetish"/>
+							<img src="1.png" className="img-fluid" width="20%" height="20%" alt="Fetish"/>
+			  				<img src="1.png" className="img-fluid" width="20%" height="20%" alt="Fetish"/>
+			  				<img src="1.png" className="img-fluid" width="20%" height="20%" alt="Fetish"/>
 			  			</div>
 					</div>
 				</div>
